30ae93103ff63cbcee275ad9e3161f7dafc2ada1
Tom Ginsberg <tom@beit.tech>
2024-09-05 22:30:43 -0400

    fixed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import BookingComponent from "./pages/Home";
 import RideBookingForm from "./pages/RideBookingForm";
